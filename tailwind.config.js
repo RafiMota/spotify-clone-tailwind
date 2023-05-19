a05b6bbc19648f1c6fda7ff8f3fd31bad9dccbf6
Rafí Mota <rafilourencom@gmail.com>
2023-05-18 23:59:15 -0300

    Many CSS changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["src/**/*.{html,js}", "*.{html,js}"],
   theme: {
+    fontFamily: {
+      'poppins': ['Poppins']
+    },
+
     extend: {
       backgroundImage: {
         'hero': 'url("/assets/hero.jpg")'
